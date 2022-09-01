2d4a97de048cab60e0288e4c47f3ba55dc5c0a68
Navskie <85003094+Navskie@users.noreply.github.com>
2022-09-02 05:14:08 +0800

    install tailwind css vite

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -5,9 +5,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Student</title>
+  @vite('resources/css/app.css')
 </head>
 <body>
-  
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <table>
     <tr>
       <th>First Name</th>
