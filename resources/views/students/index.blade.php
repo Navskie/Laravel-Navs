87029134e9cb59e773608f66825fa0c1e8689d22
Navskie <85003094+Navskie@users.noreply.github.com>
2022-09-03 20:40:15 +0800

    update table design

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -5,46 +5,46 @@
 <x-__nav :data="$array"/>
 
   <header class="max-w-lg mx-auto">
-
+  <br><br>
   </header>
-  <section class="mt-10">
-    <div class="overflow-x-auto relarive">
-      <table class="w-96 mx-auto text-sm text-left text-gray-500">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+  <section class="">
+    <div class="p-7 h-screen bg-gray-100">
+      <table class="w-1/2 mx-auto">
+        <thead class="bg-gray-50 border-b-2 border-gray-200">
           <tr>
-            <th scope="col" class="py-3 px-6">
+            <th scope="col" class="p-3 text-sm font-semi-bold tracking-wide text-left">
               First Name
             </th>
-            <th scope="col" class="py-3 px-6">
+            <th scope="col" class="p-3 text-sm font-semi-bold tracking-wide text-left">
               Last Name
             </th>
-            <th scope="col" class="py-3 px-6">
+            <th scope="col" class="p-3 text-sm font-semi-bold tracking-wide text-left">
               Gender
             </th>
-            <th scope="col" class="py-3 px-6">
+            <th scope="col" class="p-3 text-sm font-semi-bold tracking-wide text-left">
               Email
             </th>
-            <th scope="col" class="py-3 px-6">
+            <th scope="col" class="p-3 text-sm font-semi-bold tracking-wide text-left">
               age
             </th>
           </tr>
         </thead>
         <tbody>
         @foreach ($students as $student)
-          <tr class="bg-gray-800 border-b text-white">
-            <td class="py-4 px-6">
+          <tr class="bg-white text-center">
+            <td class="p-3 text-sm text-gray-700">
             {{ $student->fname }}
             </td>
-            <td class="py-4 px-6">
+            <td class="p-3 text-sm text-gray-700">
             {{ $student->lname }}
             </td>
-            <td class="py-4 px-6">
+            <td class="p-3 text-sm text-gray-700">
             {{ $student->gender }}
             </td>
-            <td class="py-4 px-6">
+            <td class="p-3 text-sm text-gray-700">
             {{ $student->email }}
             </td>
-            <td class="py-4 px-6">
+            <td class="p-3 text-sm text-gray-700">
             {{ $student->age }}
             </td>
           </tr>
